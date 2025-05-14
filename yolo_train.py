38e42c0eff94944e150c5ce8e7ca47b148632ea6
Peiyuan Li <peiyuanli3627@gmail.com>
2025-05-14 04:37:59 +0000

    update for pairwise visualization

--- yolo_train.py
+++ yolo_train.py
@@ -1,14 +1,14 @@
 from ultralytics import YOLO
 import torch
-
 model = YOLO('yolov8m.pt') 
+
 # Check if CUDA is available and configure training accordingly
 
 device = torch.device("cuda:0")
 
 if torch.cuda.is_available():
     print('Training with GPU.')
-    results = model.train(data="deep_scores.yaml", epochs=500, batch=4, imgsz=[704, 992], device=device, patience=0, deterministic=True, amp=True) # rect=True, ,[928, 1312], imgsz=[704, 992], imgsz=[1960, 2772]
+    results = model.train(data="deepscores.yaml", epochs=10, batch=3, device=device, patience=0, deterministic=True, amp=True)
     
 else:
-    results = model.train(data="deep_scores.yaml", imgsz=640, rect=True, epochs = 10, batch=4)  # patience=100, device="mps", deterministic=True, amp=True
+    results = model.train(data="deepscores.yaml", imgsz=640, rect=True, epochs = 10, batch=4)
